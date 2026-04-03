dd39fc162ff5167698180ce2b0f90fbb4c82da2a
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-03 15:56:41 +0700

    create void output

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -72,5 +72,13 @@ void mergeSort(int low, int high)
     }
 }
 
-
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
